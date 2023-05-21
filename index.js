50a31c6044e64eb66bb6cada6b32f6055c6a8bd8
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2023-05-21 09:00:19 +0600

    navbar modified

--- index.js
+++ index.js
@@ -25,14 +25,25 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    maxPoolSize: 10,
 });
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
 
-        await client.connect();
+        client.connect((error) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+        });
+
+
+
         const allToysCollection = client.db('ToysDB').collection('Toys');
 
 
